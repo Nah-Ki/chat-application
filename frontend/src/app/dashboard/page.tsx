d8ec18068c0072e3e21854247d07cd20aec981ab
mo-shahab <mohdshahabuddinsoharwardi@gmail.com>
2024-12-04 21:33:19 +0530

    feat: add the  create chat group button

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,14 +1,20 @@
 import DashNav from "@/components/dashboard/dash-nav";
 import { getServerSession } from "next-auth";
 import { authOptions, CustomSession } from "../api/auth/[...nextauth]/options";
+import CreateChatGroup from "@/components/chat-group/create-chatgroup";
 
 export default async function dashboard() {
   const session: CustomSession | null = await getServerSession(authOptions);
 
   return (
     <div>
-      <p>{JSON.stringify(session)}</p>
       <DashNav name={session?.user?.name!} image={session?.user?.image ?? undefined} />
+
+      <div className="container">
+        <div className="flex justify-end">
+          <CreateChatGroup user={session?.user}/>    
+        </div>
+      </div>
     </div>
   );
 }
