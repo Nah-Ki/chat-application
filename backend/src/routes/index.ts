c45b96c30d7739f63b0da1a6d062574f44419dfb
mo-shahab <mohdshahabuddinsoharwardi@gmail.com>
2024-12-07 18:07:43 +0530

    feat: routes and controllers of the application

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,17 +2,27 @@ import { Router } from "express";
 import authMiddleware from "../middlewares/auth-middleware.js";
 import AuthController from "../controllers/auth-controller.js";
 import ChatGroupController from "../controllers/chatgroup-controller.js";
+import ChatGroupUserController from "../controllers/chat-group-user-controller.js";
+import ChatsController from "../controllers/chats-controller.js";
 
 const router = Router();
 
 // auth routes
 router.post("/auth/login", AuthController.login);
 
-// chat gruop routes
+// chat group routes
 router.post("/chat-group", authMiddleware, ChatGroupController.store);
 router.get("/chat-group", authMiddleware, ChatGroupController.index)
-router.get("/chat-group/:id", authMiddleware, ChatGroupController.show)
+router.get("/chat-group/:id", ChatGroupController.show)
 router.put("/chat-group/:id", authMiddleware, ChatGroupController.update)
 router.delete("/chat-group/:id", authMiddleware, ChatGroupController.destroy)
+
+// chat group users routes 
+router.get("/chat-group-users", ChatGroupUserController.index);
+router.post("/chat-group-users", ChatGroupUserController.store);
+
+// chats (messages)
+router.get("/chats/:id", ChatsController.index);
+
 
 export default router;
